13388727473c6dc617d7958cfdc3c8b510929a62
Quansheng Wang <quansheng_wang@thinkyeah.com>
2025-09-09 18:26:55 +0800

    home界面.

--- app/src/main/java/com/example/mylocationtracker/fragment/HomeTabFragment.java
+++ app/src/main/java/com/example/mylocationtracker/fragment/HomeTabFragment.java
@@ -1,26 +1,132 @@
 package com.example.mylocationtracker.fragment;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
-import android.view.Gravity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.mylocationtracker.R;
+import com.example.mylocationtracker.utils.MapUtils;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.Task;
 
-public class HomeTabFragment extends Fragment {
+public class HomeTabFragment extends Fragment implements OnMapReadyCallback {
+    private static final String TAG = "HomeTabFragment";
+    private GoogleMap mMap;
+    private static final LatLng DEFAULT_LOCATION = new LatLng(35.6895, 139.6917);
+    private Location mLastKnownLocation;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    private boolean mLocationPermissionGranted;
+    private static final int DEFAULT_ZOOM = 15;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        TextView textView = new TextView(getContext());
-        textView.setText(R.string.home);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextSize(24);
-        return textView;
+        View mapView = inflater.inflate(R.layout.fragment_home, container, false);
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireActivity());
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+
+        return mapView;
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+        getLocationPermission();
+        updateLocationUI();
+        getDeviceLocation();
+    }
+
+    private final ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    mLocationPermissionGranted = true;
+                    updateLocationUI();
+                    getDeviceLocation();
+                } else {
+                    mLocationPermissionGranted = false;
+                    Log.w(TAG, "定位权限被拒绝");
+                }
+            });
+
+    private void getLocationPermission() {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(), Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = true;
+        } else {
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+    }
+
+    private void updateLocationUI() {
+        if (mMap == null) {
+            return;
+        }
+        try {
+            if (mLocationPermissionGranted) {
+                mMap.setMyLocationEnabled(true);
+                mMap.getUiSettings().setMyLocationButtonEnabled(true);
+            } else {
+                mMap.setMyLocationEnabled(false);
+                mMap.getUiSettings().setMyLocationButtonEnabled(false);
+                mLastKnownLocation = null;
+            }
+        } catch (SecurityException e) {
+            Log.e(TAG, "Exception: " + e.getMessage());
+        }
+    }
+
+    private void getDeviceLocation() {
+        try {
+            if (mLocationPermissionGranted) {
+                Task<Location> locationResult = mFusedLocationProviderClient.getLastLocation();
+                locationResult.addOnCompleteListener(requireActivity(), task -> {
+                    if (task.isSuccessful() && task.getResult() != null) {
+                        mLastKnownLocation = task.getResult();
+                        LatLng myLatLng = new LatLng(mLastKnownLocation.getLatitude(),
+                                mLastKnownLocation.getLongitude());
+                        String myAddress = MapUtils.getAddressFromLatLng(requireContext(), myLatLng);
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myLatLng, DEFAULT_ZOOM));
+                        mMap.addMarker(new MarkerOptions()
+                                        .position(myLatLng)
+                                        .title("我的位置")
+                                        .snippet(myAddress)
+                                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+//                                .showInfoWindow();
+                    } else {
+                        Log.d(TAG, "Current location is null, using default.");
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(DEFAULT_LOCATION, DEFAULT_ZOOM));
+                        mMap.addMarker(new MarkerOptions().position(DEFAULT_LOCATION).title("默认位置"));
+                    }
+                });
+            } else {
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(DEFAULT_LOCATION, DEFAULT_ZOOM));
+            }
+        } catch (SecurityException e) {
+            Log.e(TAG, "Exception: " + e.getMessage(), e);
+        }
     }
 }
