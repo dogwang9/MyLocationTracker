3ae3de1f3268e6df1d070cf740e63b6ad0f2efde
Quansheng Wang <quansheng_wang@thinkyeah.com>
2025-09-10 14:14:33 +0800

    minSDK设置降低为31.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.mylocationtracker"
-        minSdk = 33
+        minSdk = 31
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
